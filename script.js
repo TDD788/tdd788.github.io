dd547acb6233b2dc3c7f5bf42335e534a17fb202
TDD788 <thedarkdeath788@gmail.com>
2024-07-04 13:06:45 -0300

    add many things

--- script.js
+++ script.js
@@ -76,10 +76,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         datasets: [{
                             label: 'Commit Activity',
                             data: dataPoints,
-                            borderColor: 'rgba(0, 0, 0, 1)',
-                            backgroundColor: 'rgba(255, 255, 255, 0)',
+                            borderColor: 'rgba(255, 255, 255, 255)',
+                            backgroundColor: 'rgba(255, 255, 255, 255)',
                             borderWidth: 2,
-                            pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+                            pointBackgroundColor: 'rgba(255, 255, 255, 255)',
                             pointBorderWidth: 2,
                             pointRadius: 5,
                             pointHoverRadius: 7
